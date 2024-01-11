082e2957676329869b7faf3dea4abc1802d811b3
UmutKaradeniz <ukaradeniz21@gmail.com>
2024-01-11 17:13:12 +0100

    Added login Customer Check
    returns true/false based on given values

--- db_funcs.py
+++ db_funcs.py
@@ -7,9 +7,9 @@ class DBfuncs:
         cur = con.cursor()
         cur.execute("""CREATE TABLE IF NOT EXISTS customers (
                 surname TEXT,
-                user_name TEXT,
-                user_address TEXT,
-                user_postcode INTEGER,
+                name TEXT,
+                address TEXT,
+                postcode INTEGER,
                 username TEXT,
                 password TEXT                      
             )""")
@@ -35,7 +35,6 @@ class DBfuncs:
     def registerRestaurant(res_name, address, postcode, password, img_path = None):
         con = sql.connect('database.db')
         cur = con.cursor()
-        print("Connected to Database")
         sqlite_insert_blob_query = """ INSERT INTO Restaurants
                                   (res_name, address, postcode, password, picture) VALUES (?, ?, ?, ?, ?)"""
         data_tuple = (res_name, address, postcode, password, img_path)
@@ -43,8 +42,21 @@ class DBfuncs:
         con.commit()
         con.close()
 
+    #Credentials check for customer login
+    def loginCustomerCheck(username, password):
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute("SELECT EXISTS (SELECT 1 FROM customers WHERE username=? AND password=?)", (username, password))
+        result = cur.fetchone()[0]
+        return True if bool(result) == True else False
+
+            
+
 # if __name__ == "__main__":
 #     DBfuncs.createTables()
-#     DBfuncs.registerCustomer("Karadeniz2", "Umut", "ABC 6", 47055, "Umut_Karadeniz", 1234554321)
-#     DBfuncs.registerRestaurant("Al-Basha2", "ABC 7", 47055, 123321, "C:/Users/KaradenizNB/Desktop/abc.png")
-#     DBfuncs.registerRestaurant("Al-Basha3", "ABC 7", 47055, 123321)
+    # DBfuncs.registerCustomer("Karadeniz2", "Umut", "ABC 6", 47055, "Umut_Karadeniz", 1234554321)
+    # DBfuncs.registerRestaurant("Al-Basha2", "ABC 7", 47055, 123321, "C:/Users/KaradenizNB/Desktop/abc.png")
+    # DBfuncs.registerRestaurant("Al-Basha3", "ABC 7", 47055, 123321)
+    # print(DBfuncs.loginCustomerCheck('Umut_Karadeniz', 1234554321))
+    # print(DBfuncs.loginCustomerCheck('Umut_Karadeniz', 1234554322))
+    # print(DBfuncs.loginCustomerCheck('Utku', 1234554321))
