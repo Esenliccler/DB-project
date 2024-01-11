862da6e3bc21ea6f9d991a62e797e6bcd7fcd95d
UmutKaradeniz <ukaradeniz21@gmail.com>
2024-01-11 19:48:40 +0100

    added credentials checks for login restaurant

--- db_funcs.py
+++ db_funcs.py
@@ -58,17 +58,28 @@ class DBfuncs:
         cur = con.cursor()
         cur.execute("SELECT EXISTS (SELECT 1 FROM customers WHERE username=? AND password=?)", (username, password))
         result = cur.fetchone()[0]
+        con.close()
         return True if bool(result) == True else False
+    
+    #Credentials check for restaurant login
+    def loginRestaurantCheck(res_name, password):
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute("SELECT EXISTS (SELECT 1 FROM restaurants WHERE res_name=? AND password=?)", (res_name, password))
+        result = cur.fetchone()[0]  
+        con.close()
+        return True if bool(result) == True else False  
 
-            
-
-if __name__ == "__main__":
-      DBfuncs.createTables()
+# if __name__ == "__main__":
+#     DBfuncs.createTables()
 #     DBfuncs.registerCustomer("Karadeniz2", "Umut", "ABC 6", 47055, "Umut_Karadeniz", 1234554321)
 #     DBfuncs.registerCustomer("Karadeniz2", "Umut", "ABC 6", 47055, "Umut_Karadeniz1", 1234554321)
 #     DBfuncs.registerCustomer("Karadeniz2", "Umut", "ABC 6", 47055, "Umut_Karadeniz", 1234554321)
-      DBfuncs.registerRestaurant("Al-Basha2", "ABC 7", 47055, 123321, "C:/Users/KaradenizNB/Desktop/abc.png")
-      DBfuncs.registerRestaurant("Al-Basha2", "ABC 7", 47055, 123321)
+#     DBfuncs.registerRestaurant("Al-Basha2", "ABC 7", 47055, 123321, "C:/Users/KaradenizNB/Desktop/abc.png")
+#     DBfuncs.registerRestaurant("Al-Basha2", "ABC 7", 47055, 123321)
 #     print(DBfuncs.loginCustomerCheck('Umut_Karadeniz', 1234554321))
 #     print(DBfuncs.loginCustomerCheck('Umut_Karadeniz', 1234554322))
 #     print(DBfuncs.loginCustomerCheck('Utku', 1234554321))
+#     print(DBfuncs.loginRestaurantCheck('Al-Basha2', 123321))
+#     print(DBfuncs.loginRestaurantCheck('Umut_Karadeniz', 1234554322))
+#     print(DBfuncs.loginRestaurantCheck('Utku', 1234554321))
