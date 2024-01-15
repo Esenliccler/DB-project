d31b244a3f0e4d3bdc06d965d6c7c3da88f9b62f
Fadi Cheikh-el-Chabab <fadi.swat.ak@gmail.com>
2024-01-16 00:47:37 +0100

    publish branch

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -9,13 +9,16 @@ import {
   WritableSignal,
 } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 // import { AuthResponseAdapter } from 'src/app/adapters/auth/auth.adapter';
 // import { SignUpDTO } from 'src/app/DTOs/auth/sign-up.dto';
@@ -26,16 +29,37 @@ import { Router } from '@angular/router';
 // import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: 'najeb-sign-up',
+  selector: 'sign-up',
   standalone: true,
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [NgIf, ReactiveFormsModule, InputTextModule],
-  // providers: [AuthService, AuthResponseAdapter, GroupService],
+  providers: [AuthService],
 })
 export class SignUpComponent {
-  signUpForm: FormGroup;
+  confirmPasswordValidatorCustomer: ValidatorFn = (control) => {
+    if (!!this.signUpFormCustomer) {
+      const confirmPassword = control.value;
+      const { password } = this.signUpFormCustomer.value;
+      return password === confirmPassword
+        ? {}
+        : { error: 'password not confirmed' };
+    }
+    return {};
+  };
+  confirmPasswordValidatorRestaurant: ValidatorFn = (control) => {
+    if (!!this.signUpFormRestaurant) {
+      const confirmPassword = control.value;
+      const { password } = this.signUpFormRestaurant.value;
+      return password === confirmPassword
+        ? {}
+        : { error: 'password not confirmed' };
+    }
+    return {};
+  };
+  signUpFormCustomer: FormGroup;
+  signUpFormRestaurant: FormGroup;
   show: boolean = false;
   errorMessage: WritableSignal<string | null> = signal(null);
 
@@ -43,11 +67,10 @@ export class SignUpComponent {
 
   constructor(
     private readonly _fb: FormBuilder,
-    private readonly _router: Router
-  ) // private readonly _authService: AuthService,
-  // private readonly _groupService: GroupService,
-  {
-    this.signUpForm = this._fb.nonNullable.group({
+    private readonly _router: Router,
+    private readonly _authService: AuthService // private readonly _groupService: GroupService,
+  ) {
+    this.signUpFormCustomer = this._fb.nonNullable.group({
       email: this._fb.nonNullable.control<string | null>('', [
         Validators.required,
         Validators.email,
@@ -56,10 +79,10 @@ export class SignUpComponent {
         '',
         Validators.required
       ),
-      confirm: this._fb.nonNullable.control<string | null>(
-        '',
-        Validators.required
-      ),
+      confirm: this._fb.nonNullable.control<string | null>('', [
+        Validators.required,
+        this.confirmPasswordValidatorCustomer,
+      ]),
       name: this._fb.nonNullable.control<string | null>(
         '',
         Validators.required
@@ -68,65 +91,41 @@ export class SignUpComponent {
         '',
         Validators.required
       ),
-      // gender: this._fb.nonNullable.control<Gender>('MALE', Validators.required),
+      str: this._fb.nonNullable.control<string | null>('', Validators.required),
+      plz: this._fb.nonNullable.control<string | null>('', Validators.required),
+    });
+    this.signUpFormRestaurant = this._fb.nonNullable.group({
+      email: this._fb.nonNullable.control<string | null>('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: this._fb.nonNullable.control<string | null>(
+        '',
+        Validators.required
+      ),
+      confirm: this._fb.nonNullable.control<string | null>('', [
+        Validators.required,
+        this.confirmPasswordValidatorRestaurant,
+      ]),
+      name: this._fb.nonNullable.control<string | null>(
+        '',
+        Validators.required
+      ),
+      str: this._fb.nonNullable.control<string | null>('', Validators.required),
+      plz: this._fb.nonNullable.control<string | null>('', Validators.required),
     });
     localStorage.removeItem('user');
   }
 
-  public getSignUpFormControlValue(
-    fieldName: 'email' | 'password' | 'name' | 'lastName' | 'confirm'
-  ): string {
-    return this.signUpForm.value[fieldName];
+  public signUpCustomer(): void {
+    const { confirm, ...data } = this.signUpFormCustomer.value;
+    console.log(data);
+    //this._authService.signUpCustomer(data);
   }
-
-  get emailControl(): FormControl {
-    return this.signUpForm.controls['email'] as FormControl<string>;
-  }
-
-  get passwordControl(): FormControl {
-    return this.signUpForm.controls['password'] as FormControl<string>;
-  }
-
-  public signUp(): void {
-    //   // const userInputs: SignUpDTO = {
-    //     email: this.getSignUpFormControlValue('email'),
-    //     password: this.getSignUpFormControlValue('password'),
-    //     name: this.getSignUpFormControlValue('name'),
-    //     lastName: this.getSignUpFormControlValue('lastName'),
-    //     gender: 'MALE',
-    //     birthday: '1999-01-01T10:10:10.000Z',
-    //     phone: '963912343533',
-    //   };
-    //   this.loadingButton.set(true);
-    //   // this._authService
-    //     // .signUp(userInputs)
-    //     // .pipe(
-    //       tap((user: AuthResponseModel) => {
-    //         localStorage.setItem('user', JSON.stringify(user));
-    //         this._groupService
-    //           .createGroup(this.getSignUpFormControlValue('name'))
-    //           .pipe(
-    //             take(1),
-    //             tap((group: { id: number; name: string; creatorId: number }) => {
-    //               this._groupService
-    //                 .addUsersToGroup(group.id, [group.creatorId])
-    //                 .pipe(take(1))
-    //                 .subscribe();
-    //             }),
-    //           )
-    //           .subscribe();
-    //         this.loadingButton.set(false);
-    //         this._router.navigate(['home']);
-    //       }),
-    //       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    //       catchError((error: any): Observable<any> => {
-    //         this.loadingButton.set(false);
-    //         this.errorMessage.set(error.error.message);
-    //         return of({});
-    //       }),
-    //       take(1),
-    //     )
-    //     .subscribe();
+  public signUpRestaurant(): void {
+    const { confirm, ...data } = this.signUpFormRestaurant.value;
+    console.log(data);
+    //this._authService.signUpRestaurant(data);
   }
 
   public showPassword(): void {
